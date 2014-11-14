a053f3ed2d95b3071bc2556962a411c43ae46bd3
Owen Synge <osynge@suse.com>
2014-11-14 14:05:24 +0100

    some fixes so 'python setup.py bdist_rpm' works

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@
 
 from distutils.core import setup
 
+f = open('README.md')
+
 setup(name='odpgenerator',
       version='0.1',
       description='Generate OpenDocument Presentation (odp) files from markdown',
       author='Thorsten Behrens',
       author_email='tbehrens@acm.org',
       url='https://github.com/thorstenb/odpgen.git',
-      long_description=fread('README.md'),
-      py_modules=['odpgenerator']
+      long_description=f.read(),
+      py_modules=['odpgenerator'],
+      scripts=['odpgenerator'],
      )
